59115c763d7ac0d609b5a8ee074b38a11d9f7631
matEhickey <mathias.didier7@wanadoo.fr>
2020-10-16 17:02:27 +0200

    remove chrome only (deprecated way to do it)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $( document ).ready( function() {
     console.log("ready");
-    
+
     actualCMD = "";
     cursorPos = 0;
     historyCMD = [];
     historyPos = -1;
     outputsCMD = [];
-    
+
     myCopyPaste = null;
 
     inputCMD = document.getElementById("cmdInput");
@@ -14,57 +14,33 @@ $( document ).ready( function() {
     debugCMD = document.getElementById("debug");
     GLOBALNETWORK = new GlobalNetwork(); //GLOBAL
     loadGame();
-    
-    
-    
-    
-    
+
+
+
+
+
     $('#webNavModal').draggable({
                   handle: ".modal-header"
               });
-              
+
               $('#geditModal').draggable({
                   handle: ".modal-header"
               });
-    
+
 });
 
 $( window ).on( "load", function() {
-    console.log("loaded");
-    
-
-    //document.getElementById('browserAlert').hidden = isChrome;
-    //document.getElementById("loadingModalTitle").hidden = !isChrome;
-    
-    
-    
-    
     setTimeout(function(){
-        if(isChrome){
-            
-            //document.getElementById("main").hidden = false;
-            
-              
-            $( "#loadingModal" ).fadeOut( "slow" );
-            
-            setTimeout(function(){
-                $( "#main" ).fadeIn( "slow" );
-                $('#loadingModal').modal('hide');
-                //showWebNav();
-                showPrompt();
-                inputCMD.focus();
-            },700);
-            
-            
-        }
-        else{
-            
-        }
-        
+      $( "#loadingModal" ).fadeOut( "slow" );
+
+      setTimeout(function(){
+          $( "#main" ).fadeIn( "slow" );
+          $('#loadingModal').modal('hide');
+          //showWebNav();
+          showPrompt();
+          inputCMD.focus();
+      },700);
     },400);
-    
-
-
 });
 
 
@@ -73,7 +49,7 @@ function onresize(){
     console.log("onresize");
     var height = $(window).height()
     if(height < 600){
-        
+
     }
 }
 
@@ -139,7 +115,7 @@ function initComputers(){  // never called, used for dev
     initStandartComputer(myPC);
     var readme = new File("README");
     myPC.rootFolder.addFile(readme);
-    
+
     readme.setText(`
         Hello, welcome in datTerm.
 
@@ -153,40 +129,40 @@ function initComputers(){  // never called, used for dev
 
         You can view the MANUAL by typing \"man\", or \"help\".
 
-        When you'r ready to begin the game, just send me an email filled with the content of the file \"/tmp/sendMeThatFile.txt\" 
+        When you'r ready to begin the game, just send me an email filled with the content of the file \"/tmp/sendMeThatFile.txt\"
         I'll responds to your mail box (<computer>/mails) as soon as  you send me that file.
         My name is kuzco, bye =)
     `);
-    
+
     var toSendFile = new File("sendMeThatFile.txt");
     var kuzcoNeededFile1 = "I'm ready kuzco !";
     toSendFile.setText(kuzcoNeededFile1);
     var tmpFolder = myPC.rootFolder.getFileByName("tmp");
     if(tmpFolder){ tmpFolder.addFile(toSendFile) }
-    
-    
-    
+
+
+
     var hackingNeighbour = new Computer("pr4nk573r","144.155.30.200",myNetwork,"admin");
     initStandartComputer(hackingNeighbour);
-    
+
     var myDumpNeighbourNetwork = new Network();
     var myDumpNeighbourPC = new Computer("user1","1.2.3.4",myDumpNeighbourNetwork,"mdp");
     initStandartComputer(myDumpNeighbourPC);
-    
+
     var kuzcoNetwork = new Network();
     var kuzcoPC = new Computer("kuzco","124.26.48.95",kuzcoNetwork,"mdp");
     initStandartComputer(kuzcoPC);
-    
-    
+
+
     GLOBALNETWORK.addNetwork(myDumpNeighbourNetwork);
     GLOBALNETWORK.addNetwork(myNetwork);
     GLOBALNETWORK.addNetwork(kuzcoNetwork);
-    
+
     currentComputer = GLOBALNETWORK.getMyComputer(); //GLOBAL
 
 }
 
-function loadGame(reset){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+function loadGame(reset){
     var init = `{"GlobalNetwork":{"nets":[{"network":{"comps":[{"name":"user1","ip":"1.2.3.4","mdp":"mdp","rootFolder":{"folder":"/","files":[{"folder":"tmp","files":[{"file":"tmp1","text":"0.6914791499797186"},{"file":"tmp2","text":"0.34644155864990345"}]},{"folder":"mails","files":[]},{"folder":"sys","files":[{"folder":"boot","files":[]},{"folder":"data","files":[]},{"folder":"etc","files":[]},{"folder":"usr","files":[]},{"folder":"var","files":[]}]},{"folder":"logs","files":[]}]}}]}},{"network":{"comps":[{"name":"myComputer","ip":"144.155.524.233","mdp":"mdp","rootFolder":{"folder":"/","files":[{"folder":"tmp","files":[{"file":"tmp1","text":"0.9374491427197225"},{"file":"tmp2","text":"0.9999550790963128"},{"file":"sendMeThatFile.txt","text":"I'm ready kuzco !"}]},{"folder":"mails","files":[]},{"folder":"sys","files":[{"folder":"boot","files":[]},{"folder":"data","files":[]},{"folder":"etc","files":[]},{"folder":"usr","files":[]},{"folder":"var","files":[]}]},{"folder":"logs","files":[{"file":"log_modifingFile0","text":"2/1 23:9:23\\nSaved File (/web/index.html)"}]},{"file":"README","text":"Hello, welcome in datTerm.\\n\\nYou can view the help by typing \\\"help\\\" or \\\"man\\\" in the terminal.\\nThe game is about to begin, but before, I'll make sure you well understand the basic commands you'll need.\\n\\nYou can view the name of the folder you'r in by typing \\\"pwd\\\".\\nYou can list the files in by typing \\\"ls\\\".\\nYou can see a file by typing \\\"cat \\\" -> ex: cat README\\nYou can view the MANUAL by typing \\\"man\\\", or \\\"help\\\".\\n\\nWhen you'r ready to begin the game, just send me an email filled with the content of the file \\\"/tmp/sendMeThatFile.txt\\\"\\nI'll responds to your mail box (<computer>/mails) as soon as  you send me that file.\\n\\nAlso, my name is kuzco, bye =)"},{"folder":"web","files":[{"file":"index.html","text":"\\n<!DOCTYPE html>\\n<html lang=\\\"en\\\">\\n<head>\\n\\n</head>\\n<body>\\nHello in the web server application\\n</div>\\n</div>\\n\\n</body>\\n</html>\\n"}]}]}},{"name":"pr4nk573r","ip":"144.155.30.200","mdp":"admin","rootFolder":{"folder":"/","files":[{"folder":"tmp","files":[{"file":"tmp1","text":"0.2571366804991664"},{"file":"tmp2","text":"0.5421219127044794"}]},{"folder":"mails","files":[]},{"folder":"sys","files":[{"folder":"boot","files":[]},{"folder":"data","files":[]},{"folder":"etc","files":[]},{"folder":"usr","files":[]},{"folder":"var","files":[]}]},{"folder":"logs","files":[]}]}}]}},{"network":{"comps":[{"name":"kuzco","ip":"124.26.48.95","mdp":"mdp","rootFolder":{"folder":"/","files":[{"folder":"tmp","files":[{"file":"tmp1","text":"0.3856929955513957"},{"file":"tmp2","text":"0.19420347775928382"}]},{"folder":"mails","files":[]},{"folder":"sys","files":[{"folder":"boot","files":[]},{"folder":"data","files":[]},{"folder":"etc","files":[]},{"folder":"usr","files":[]},{"folder":"var","files":[]}]},{"folder":"logs","files":[]}]}}]}}]}}`;
 
     var saved = localStorage.getItem("saved");
@@ -198,11 +174,11 @@ function loadGame(reset){
         var newGlobal = GlobalNetwork.fromJSON(init);
         progression = 0;
     }
-    
+
     notes = new AppliNotes();
     AppliNotes.addNote("Welcome to datTerm");
     AppliNotes.addNote("Try to type \"cat README\" ");
-    
+
     GLOBALNETWORK = newGlobal;
     currentComputer = GLOBALNETWORK.getMyComputer();
 }
